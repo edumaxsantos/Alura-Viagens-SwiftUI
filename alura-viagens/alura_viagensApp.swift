e8983ecc01a26a1180ee61051bd7e4a8bd19b545
edumaxsantos@gmail.com <edumaxsantos@gmail.com>
2021-06-27 17:58:29 -0300

    Initial Commit

---
+++ alura-viagens/alura_viagensApp.swift
@@ -0,0 +1,17 @@
+//
+//  alura_viagensApp.swift
+//  alura-viagens
+//
+//  Created by Eduardo Santos on 27/06/21.
+//
+
+import SwiftUI
+
+@main
+struct alura_viagensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
