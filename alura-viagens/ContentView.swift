0f90b6742453e93bcad3829d8a2f7c770d8c29c6
edumaxsantos@gmail.com <edumaxsantos@gmail.com>
2021-06-27 19:41:34 -0300

    Criado o header já com estilos

--- alura-viagens/ContentView.swift
+++ alura-viagens/ContentView.swift
@@ -9,18 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("alura viagens")
-            Text("ESPECIAL")
-            Text("BRASIL")
-            
-            List {
-                Text("Rio de Janeiro")
-                Text("Ceará")
-                Text("Atibaia")
-                Text("Rio de Janeiro")
+        
+        GeometryReader { view in
+            // MARK: - VStack Principal
+            VStack {
+                 // MARK: - VStack Header
+                VStack {
+                    Text("alura viagens")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Black", size: 20))
+                        .padding(.top, 50)
+                    Text("ESPECIAL")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Book", size: 20))
+                        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                        .padding(.leading, 30)
+                    Text("BRASIL")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Black", size: 23))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                    
+                    
+                }
+                .frame(width: view.size.width, height: 180, alignment: .top)
+                .background(Color.purple)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                
+                
+                List {
+                    Text("Rio de Janeiro")
+                    Text("Ceará")
+                    Text("Atibaia")
+                    Text("Rio de Janeiro")
+                }
             }
         }
+        
+        
     }
 }
 
