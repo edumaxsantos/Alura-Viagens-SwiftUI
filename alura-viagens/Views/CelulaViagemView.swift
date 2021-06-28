a58b7e5557aca6b0d189119289446f2aba29bc61
edumaxsantos@gmail.com <edumaxsantos@gmail.com>
2021-06-28 00:10:12 -0300

    Ajustado imagem no iPad

--- alura-viagens/Views/CelulaViagemView.swift
+++ alura-viagens/Views/CelulaViagemView.swift
@@ -10,17 +10,29 @@ import SwiftUI
 struct CelulaViagemView: View {
     
     var viagem: Viagem
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    
+    
+    private func isCompact(yes compactValue: CGFloat, no regularValue: CGFloat) -> CGFloat {
+        return self.horizontalSizeClass == .compact ? compactValue : regularValue
+    }
     
     var body: some View {
         VStack(alignment: .leading) {
             Text(viagem.titulo)
+                .font(.custom("Avenir", size: isCompact(yes: 14, no: 24)))
             Image(viagem.imagem)
                 .resizable()
-                .frame(height: 125)
+                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                .frame(height: isCompact(yes: 125, no: 200))
+                .clipped()
+                
             HStack {
                 Text(viagem.quantidadeDeDias)
+                    .font(.custom("Avenir", size: isCompact(yes: 14, no: 24)))
                 Spacer()
                 Text(viagem.valor)
+                    .font(.custom("Avenir", size: isCompact(yes: 14, no: 24)))
             }
         }
     }
