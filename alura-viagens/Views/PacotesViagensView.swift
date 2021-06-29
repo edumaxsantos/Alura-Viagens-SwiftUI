ae8e4fc1f5658dc0a09343d690875ce05a905cd1
edumaxsantos@gmail.com <edumaxsantos@gmail.com>
2021-06-28 23:04:43 -0300

    Configurado layout do card de pacotes viagens

--- alura-viagens/Views/PacotesViagensView.swift
+++ alura-viagens/Views/PacotesViagensView.swift
@@ -8,22 +8,46 @@
 import SwiftUI
 
 struct PacotesViagensView: View {
-    var body: some View {
-        VStack {
-            Image("nome-da-imagem")
-            Text("Nova York")
-            Text("Aéreo + Hotel")
-            Text("01 de mar ~ 30 de jul")
-            Text("a partir de")
-            Text("R$ 3.325")
-            Text("em até 12x")
-            Text("cancelamento Grátis")
+    var body: some View {        
+        GeometryReader { view in
+            VStack {
+                Image("nova-york-1")
+                    .resizable()
+                    .frame(height: 135)
+                    .aspectRatio(contentMode: .fill)
+                    .clipped()
+                Text("Nova York")
+                    .font(.custom("Avenir Black", size: 12))
+                    .frame(minWidth: 0, maxWidth: view.size.width, alignment: .leading)
+                Text("Aéreo + Hotel")
+                    .font(.custom("Avenir", size: 10))
+                    .frame(minWidth: 0, maxWidth: view.size.width, alignment: .leading)
+                Text("01 de mar ~ 30 de jul")
+                    .font(.custom("Avenir", size: 10))
+                    .frame(minWidth: 0, maxWidth: view.size.width, alignment: .leading)
+                Text("a partir de")
+                    .font(.custom("Avenir", size: 9))
+                    .frame(minWidth: 0, maxWidth: view.size.width, alignment: .trailing)
+                Text("R$ 3.325")
+                    .font(.custom("Avenir Black", size: 16))
+                    .frame(minWidth: 0, maxWidth: view.size.width, alignment: .trailing)
+                    .foregroundColor(.orange)
+                Text("em até 12x")
+                    .font(.custom("Avenir", size: 9))
+                    .frame(minWidth: 0, maxWidth: view.size.width, alignment: .trailing)
+                Text("cancelamento Grátis")
+                    .font(.custom("Avenir", size: 9))
+                    .frame(minWidth: 0, maxWidth: view.size.width, alignment: .trailing)
+                    .foregroundColor(.green)
+            }
         }
+        .frame(width: 180, height: 250)
     }
 }
 
 struct PacotesViagensView_Previews: PreviewProvider {
     static var previews: some View {
         PacotesViagensView()
+            .previewLayout(PreviewLayout.fixed(width: 180, height: 250))
     }
 }
